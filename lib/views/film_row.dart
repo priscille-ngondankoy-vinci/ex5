fed2773d164ef3b2b7a77e33e37e19d09cc3b070
Priscille Ngondankoy Nkoy <priscille.ngondankoy@student.vinci.be>
2026-04-03 09:38:31 +0200

    T09.2 Affichage des films

--- lib/views/film_row.dart
+++ lib/views/film_row.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../models/film.dart';
 
@@ -10,12 +11,22 @@ class FilmRow extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text(film.title),
+      title: Text(film.title, style: TextStyle(fontStyle: FontStyle.italic, )),
       subtitle: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text("Description :  ${film.description} - ${film.running_time} minutes ,  "
-              "Directed by : ${film.director}"),
+          Text("Release on ${film.release_date}", style: TextStyle(color: Colors.cyan, fontWeight: FontWeight.bold)),
+          Text("Description :  ${film.description}  "
+              , style: TextStyle(color: Colors.green, fontWeight: FontWeight.bold)) ,
+          Text(
+          "Directed by : ${film.director}", style: TextStyle(color: Colors.pinkAccent, fontWeight: FontWeight.bold)),
+  Text("Duration : ${film.running_time}", style: TextStyle(color: Colors.deepOrangeAccent, fontWeight: FontWeight.bold))
+,
+          Image.network(
+            '${film.image}',
+            width: 200,
+            height: 200
+          )
 
         ],
       ),
